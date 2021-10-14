2130bc38984140c736f83f4fa9c21e3b2e548eee
Rafaela de Moraes Papale <rafaela.papale@gec.inatel.br>
2021-10-14 10:21:07 -0300

    Implementando pagina de favoritos

--- src/pages/favoritos/index.js
+++ src/pages/favoritos/index.js
@@ -1,10 +1,41 @@
 import './style.css';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Favoritos() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || []);
+    }, []);
+
+    function excluirFilme(idFilme){
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== idFilme)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes));
+    }
 
     return (
-        <div className="filme-info">
-            <h1>Page 123</h1>
+        <div className="fav">
+            <h1>Meus filmes</h1>
+            <ul>
+                {filmes.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            <span>{item.nome}</span>
+
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={ () => excluirFilme(item.id) }>Excluir</button>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
